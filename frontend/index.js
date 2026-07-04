2beb2a71c81efdbb71d9ff532899bb60e8d091b7
Anjali Jakka <jakkaanjali07@gmail.com>
2026-07-04 09:09:14 +0530

    updated 1st day work

--- frontend/index.js
+++ frontend/index.js
@@ -1,2 +1,29 @@
-console.log("Hello from js");
-alert("hello from JS");
+const users = [
+    {
+        name: "Jane Doe",
+        gender: "Female",
+        image: "./images/jane.png"
+    },
+    {
+        name: "John Doe",
+        gender: "Male",
+        image: "./images/john.png"
+    }
+];
+
+let curUserId = 0;
+
+function toggle() {
+
+    curUserId = (curUserId + 1) % users.length;
+
+    document.getElementById("img").src = users[curUserId].image;
+
+    document.getElementById("card-name").innerText =
+        users[curUserId].name;
+
+    document.getElementById("card-gender").innerText =
+        users[curUserId].gender;
+}
+
+console.log("Hello from JavaScript");
